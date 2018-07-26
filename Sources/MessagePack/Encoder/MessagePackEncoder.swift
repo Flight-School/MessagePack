fc7bcb01a34f3334d512ec70dadce344a05f3386
Mattt <mattt@me.com>
2018-07-26 10:21:38 -0700

    Add public initializer and public access to encode method

--- Sources/MessagePack/Encoder/MessagePackEncoder.swift
+++ Sources/MessagePack/Encoder/MessagePackEncoder.swift
@@ -4,7 +4,9 @@ import Foundation
  
  */
 public class MessagePackEncoder {
-    func encode(_ value: Encodable) throws -> Data {
+    public init() {}
+    
+    public func encode(_ value: Encodable) throws -> Data {
         let encoder = _MessagePackEncoder()
         try value.encode(to: encoder)
         return encoder.data
