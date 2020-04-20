ea754a0c1852db18a38a89f413f63afd529e9eed
Mattt <mattt@me.com>
2020-04-20 06:02:01 -0700

    Add instrumentApproachProcedures property to Airport
    KPDX has an instrumentApproachProcedures property with more than 16 values, which forces a variable array

--- Tests/MessagePackTests/Airport.swift
+++ Tests/MessagePackTests/Airport.swift
@@ -3,7 +3,7 @@ struct Airport: Codable, Equatable {
     let iata: String
     let icao: String
     let coordinates: [Double]
-    
+
     struct Runway: Codable, Equatable {
         enum Surface: String, Codable, Equatable {
             case rigid, flexible, gravel, sealed, unpaved, other
@@ -15,6 +15,8 @@ struct Airport: Codable, Equatable {
     }
     
     let runways: [Runway]
+
+    let instrumentApproachProcedures: [String]
     
     static var example: Airport {
         return Airport(
@@ -29,6 +31,32 @@ struct Airport: Codable, Equatable {
                     distance: 1829,
                     surface: .flexible
                 )
+            ],
+            instrumentApproachProcedures: [
+                "HI-ILS OR LOC RWY 28",
+                "HI-ILS OR LOC/DME RWY 10",
+                "ILS OR LOC RWY 10L",
+                "ILS OR LOC RWY 10R",
+                "ILS OR LOC RWY 28L",
+                "ILS OR LOC RWY 28R",
+                "ILS RWY 10R (SA CAT I)",
+                "ILS RWY 10R (CAT II - III)",
+                "RNAV (RNP) Y RWY 28L",
+                "RNAV (RNP) Y RWY 28R",
+                "RNAV (RNP) Z RWY 10L",
+                "RNAV (RNP) Z RWY 10R",
+                "RNAV (RNP) Z RWY 28L",
+                "RNAV (RNP) Z RWY 28R",
+                "RNAV (GPS) X RWY 28L",
+                "RNAV (GPS) X RWY 28R",
+                "RNAV (GPS) Y RWY 10L",
+                "RNAV (GPS) Y RWY 10R",
+                "LOC/DME RWY 21",
+                "VOR-A",
+                "HI-TACAN RWY 10",
+                "TACAN RWY 28",
+                "COLUMBIA VISUAL RWY 10L/",
+                "MILL VISUAL RWY 28L/R"
             ]
         )
     }
